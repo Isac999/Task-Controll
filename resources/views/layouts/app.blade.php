44cad856ce312978f7d101404d9e17e41f55d3b6
Isac999 <fisac0040@gmail.com>
2022-10-05 11:42:24 -0300

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,10 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item"> 
+                                <a href="{{ route('tarefa.index') }}" class="nav-link"> Tarefas </a>
+                            </li>
+                            
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
--- resources/views/tarefas/index.blade.php
+++ resources/views/tarefas/index.blade.php
@@ -5,7 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"> Tarefas </div>
+                <div class="card-header"> 
+                    Tarefas 
+                    <a href="{{ route('tarefa.create') }}" style="float: right"> Nova Tarefa</a>
+                </div>
                 <div class="card-body">
                     <table class="table">
                         <thead>
